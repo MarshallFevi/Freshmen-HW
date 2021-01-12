5af890246a371b32dc3ec245af78bd82b75b8759
MarshallFevi <77315924+MarshallFevi@users.noreply.github.com>
2021-01-12 23:10:29 +0000

    Create Hard Code.cpp

---
+++ CS 1336/Assignment 1/Hard Code.cpp
@@ -0,0 +1,35 @@
+/*
+Module Name: Hello World Diamond
+Date Written: 9/2/2020 
+Author: ******
+Module Design Description:
+	Create a diamond with the text" Hello Comet" in the middle.
+Outputs:
+	Diamond with the height of 16 and "Hello Comet" in the middle.
+*/
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Temoc, Enarc and Scorcher say :" << endl << endl;
+	 
+	cout << "        *" << endl;
+	cout << "       * *" << endl;
+	cout << "      *   *" << endl;
+	cout << "     *     *" << endl;
+	cout << "    *       *" << endl;
+	cout << "   *         *" << endl;
+	cout << "  *           *" << endl;
+	cout << " * Hello Comet *" << endl;
+	cout << "  *           *" << endl;
+	cout << "   *         *" << endl;
+	cout << "    *       *" << endl;
+	cout << "     *     *" << endl;
+	cout << "      *   *" << endl;
+	cout << "       * *" << endl;
+	cout << "        *" << endl;
+	
+	return 0;
+}
